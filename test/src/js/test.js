343f8222dc70ad65769fc49e5b330c16ecbed8db
Ryan Deniega <103834718+SemiAutomat1c@users.noreply.github.com>
2025-05-12 17:24:06 +0800

    Add test script and debugging for JavaScript issues

---
+++ test/src/js/test.js
@@ -0,0 +1,6 @@
+// Simple test script
+console.log('Test script loaded!');
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM fully loaded');
+    alert('JavaScript is working!');
+}); 
